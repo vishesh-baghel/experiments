8d1a5de34478995764c515b06b7e478f011e0dd0
vishesh <visheshbaghel99@gmail.com>
2025-12-21 17:34:37 +0530

    replaced jsdom to fix the test failures in github actions

--- packages/squad/vitest.config.ts
+++ packages/squad/vitest.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
     globals: true,
     setupFiles: ["./src/__tests__/setup.ts"],
     include: ["src/**/*.test.{ts,tsx}"],
